4d4d02865ac1572a0ff467c747d70f771de0defb
Fabrice Daugan <fabrice.daugan@gfi.fr>
2017-03-26 16:12:58 +0200

    Replace "plugin.dir" by "plugin-home"

--- app-business/src/main/java/org/ligoj/app/loader/PluginsClassLoader.java
+++ app-business/src/main/java/org/ligoj/app/loader/PluginsClassLoader.java
@@ -8,37 +8,76 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.ligoj.bootstrap.core.resource.TechnicalException;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Class Loader which load jars in 'plugins.dir' directory
+ * Class Loader which load jars in {@value #PLUGINS_DIR} directory inside the home directory.
  */
 @Slf4j
 public class PluginsClassLoader extends URLClassLoader {
 
-	/**
-	 * Plug-ins directory system property
-	 */
-	public static final String PLUGINS_DIR = "plugins.dir";
+	private final Path homeDirectory;
 
 	/**
-	 * default constructor
+	 * The plug-in directory
+	 */
+	private Path pluginDirectory;
+
+	/**
+	 * Plug-ins directory inside the home property
+	 */
+	public static final String PLUGINS_DIR = "plugins";
+
+	/**
+	 * Default home directory part used in addition of system user home directory : "/home/my-user" for sample.
+	 */
+	public static final String HOME_DIR_FOLDER = ".ligoj";
+
+	/**
+	 * System property name pointing to the home directory. When undefined, system user home directory will be used
+	 */
+	public static final String HOME_DIR_PROPERTY = "ligoj.home";
+
+	/**
+	 * Initialize the plugin {@link URLClassLoader} and the related directories.
 	 * 
 	 * @throws IOException
 	 *             exception when reading plugins directory
 	 */
 	public PluginsClassLoader() throws IOException {
 		super(new URL[0], Thread.currentThread().getContextClassLoader());
+		this.homeDirectory = computeHome();
+		this.pluginDirectory = this.homeDirectory.resolve(PLUGINS_DIR);
+
+		// Create the plug-in directory as needed
+		log.info("Initialize the plug-ins from directory from {}", homeDirectory);
+		Files.createDirectories(this.pluginDirectory);
+
 		log.info("Load plugins throught Custom Plugins ClassLoader");
-		final String pluginsDir = System.getProperty(PLUGINS_DIR);
-		if (pluginsDir == null) {
-			log.error("Plugins Directory (system property 'plugins.dir') is missing.");
-			throw new TechnicalException("Plugins Directory is missing.");
-		}
-		for (final URI uri : Files.list(Paths.get(pluginsDir)).filter(p -> p.toString().endsWith(".jar")).map(Path::toUri).toArray(URI[]::new)) {
+		for (final URI uri : Files.list(this.pluginDirectory).filter(p -> p.toString().endsWith(".jar")).map(Path::toUri).toArray(URI[]::new)) {
+			log.info("Add plugin {}", uri);
 			addURL(uri.toURL());
 		}
+	}
+
+	/**
+	 * Compute the right home directory for the application from the system properties.
+	 * 
+	 * @return The computed home directory.
+	 */
+	protected Path computeHome() {
+		final Path homeDir;
+		if (System.getProperty(HOME_DIR_PROPERTY) == null) {
+			// Non standard home directory
+			homeDir = Paths.get(System.getProperty("user.home"), HOME_DIR_FOLDER);
+			log.info("Home directory is '{}', resolved from current home user location. Use '{}' system property to override this path", homeDir,
+					HOME_DIR_PROPERTY);
+		} else {
+			// Home directory inside the system user's home directory
+			homeDir = Paths.get(System.getProperty(HOME_DIR_PROPERTY));
+			log.info("Home directory is '{}', resolved from the system property '{}'", homeDir, HOME_DIR_PROPERTY);
+		}
+		return homeDir;
 	}
 
 }
--- app-business/src/test/java/org/ligoj/app/loader/PluginsClassLoaderTest.java
+++ app-business/src/test/java/org/ligoj/app/loader/PluginsClassLoaderTest.java
@@ -1,27 +1,64 @@
 package org.ligoj.app.loader;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
-import org.ligoj.bootstrap.core.resource.TechnicalException;
-
+/**
+ * Test class of {@link PluginsClassLoader}
+ */
 public class PluginsClassLoaderTest {
 
-	private static final String PLUGINS_DIRECTORY = "./target/test-classes/org/ligoj/saas/loader";
+	private static final String USER_HOME_DIRECTORY = "target/test-classes/home-test";
 
 	@Test
-	public void jarsAreLoadedByClassLoader() throws Exception {
-		System.setProperty(PluginsClassLoader.PLUGINS_DIR, PLUGINS_DIRECTORY);
-		final PluginsClassLoader classLoader = new PluginsClassLoader();
-		Assert.assertEquals(1, classLoader.getURLs().length);
-		Assert.assertTrue(classLoader.getURLs()[0].getFile().endsWith("junit.jar"));
-		classLoader.close();
+	public void standardHome() throws Exception {
+		PluginsClassLoader classLoader = null;
+		final String oldHome = System.getProperty("user.home");
+		try {
+			System.setProperty("user.home", USER_HOME_DIRECTORY);
+			classLoader = checkClassLoader();
+		} finally {
+			System.setProperty("user.home", oldHome);
+			IOUtils.closeQuietly(classLoader);
+		}
 	}
 
-	@Test(expected = TechnicalException.class)
-	public void PluginsDirSystemPropertyMustBeDefined() throws Exception {
-		final PluginsClassLoader classLoader = new PluginsClassLoader();
-		classLoader.close();
+	@Test
+	public void forcedHome() throws Exception {
+		PluginsClassLoader classLoader = null;
+		try {
+			System.setProperty("ligoj.home", USER_HOME_DIRECTORY + "/.ligoj");
+			classLoader = checkClassLoader();
+		} finally {
+			System.clearProperty("ligoj.home");
+			IOUtils.closeQuietly(classLoader);
+		}
+	}
+
+	private PluginsClassLoader checkClassLoader() throws IOException {
+		PluginsClassLoader classLoader;
+		classLoader = new PluginsClassLoader();
+		Assert.assertEquals(1, classLoader.getURLs().length);
+		
+		// Check the plug-in is in the class-path
+		final URL pluginTestUrl = classLoader.getURLs()[0];
+		Assert.assertTrue(pluginTestUrl.getFile().endsWith("plugin-foo.jar"));
+		
+		// Check the JAR is readable
+		final InputStream pluginTestUrlStream = pluginTestUrl.openStream();
+		Assert.assertNotNull(pluginTestUrlStream);
+		IOUtils.closeQuietly(pluginTestUrlStream);
+		
+		// Check the content of the plug-in is resolvable from the class loader
+		IOUtils.toString(classLoader.getResourceAsStream("home-test/.ligoj/plugins/plugin-foo.jar"), StandardCharsets.UTF_8.name());
+		Assert.assertEquals("BAR", IOUtils.toString(classLoader.getResourceAsStream("plugin-foo.txt"), StandardCharsets.UTF_8.name()));
+		return classLoader;
 	}
 
 }
