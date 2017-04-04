c07d698f20d4d9a8c94a2ffcc543d1b018ec4047
Fabrice Daugan <fabrice.daugan@gmail.com>
2017-04-05 00:58:35 +0200

    Add CI configuration

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# ligoj
-Main ligoj Application
+## :link: Ligoj [![Maven Central](https://maven-badges.herokuapp.com/maven-central/org.ligoj.app/root/badge.svg)](https://maven-badges.herokuapp.com/maven-central/org.ligoj.app/root)
+API plugin used for compatibility check
+
+[![Build Status](https://travis-ci.org/ligoj/ligoj.svg?branch=master)](https://travis-ci.org/ligoj/ligoj)
+[![Build Status](https://circleci.com/gh/ligoj/ligoj.svg?style=svg)](https://circleci.com/gh/ligoj/ligoj)
+[![Build Status](https://codeship.com/projects/59d0b6a0-ef12-0134-dc5d-06835e321a69/status?branch=master)](https://codeship.com/projects/208765)
+[![Build Status](https://semaphoreci.com/api/v1/ligoj/ligoj/branches/master/shields_badge.svg)](https://semaphoreci.com/ligoj/ligoj)
+[![Build Status](https://ci.appveyor.com/api/projects/status/5926fmf0p5qp9j16/branch/master?svg=true)](https://ci.appveyor.com/project/ligoj/ligoj/branch/master)
+[![Coverage Status](https://coveralls.io/repos/github/ligoj/ligoj/badge.svg?branch=master)](https://coveralls.io/github/ligoj/ligoj?branch=master)
+[![Dependency Status](https://www.versioneye.com/user/projects/58caeda8dcaf9e0041b5b978/badge.svg?style=flat-square)](https://www.versioneye.com/user/projects/58caeda8dcaf9e0041b5b978)
+[![Quality Gate](https://sonarqube.com/api/badges/gate?key=org.ligoj.api:root)](https://sonarqube.com/dashboard/index/org.ligoj.api:root)
+[![Sourcegraph Badge](https://sourcegraph.com/github.com/ligoj/ligoj/-/badge.svg)](https://sourcegraph.com/github.com/ligoj/ligoj?badge)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/abf810c094e44c0691f71174c707d6ed)](https://www.codacy.com/app/ligoj/ligoj?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=ligoj/ligoj&amp;utm_campaign=Badge_Grade)
+[![CodeFactor](https://www.codefactor.io/repository/github/ligoj/ligoj/badge)](https://www.codefactor.io/repository/github/ligoj/ligoj)
+[![License](http://img.shields.io/:license-mit-blue.svg)](http://gus.mit-license.org/)
---
+++ travis.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+set -euo pipefail
+
+#
+# A (too) old version of JDK8 is installed by default on Travis.
+# This method is preferred over Travis apt oracle-java8-installer because
+# JDK is kept in cache. It does not need to be downloaded from Oracle
+# at each build.
+#
+function installJdk8 {
+  echo "Setup JDK 1.8u121"
+  mkdir -p ~/jvm
+  pushd ~/jvm > /dev/null
+  if [ ! -d "jdk1.8.0_121" ]; then
+    echo "Download JDK8"
+    wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz
+    tar xzf jdk-8u121-linux-x64.tar.gz
+    rm jdk-8u121-linux-x64.tar.gz
+  fi
+  popd > /dev/null
+  export JAVA_HOME=~/jvm/jdk1.8.0_121
+  export PATH=$JAVA_HOME/bin:$PATH
+}
+
+#
+# Maven 3.2.5 is installed by default on Travis. Maven 3.3.9 is preferred.
+#
+function installMaven {
+  echo "Setup Maven"
+  mkdir -p ~/maven
+  pushd ~/maven > /dev/null
+  if [ ! -d "apache-maven-3.3.9" ]; then
+    echo "Download Maven 3.3.9"
+    curl -sSL http://apache.mirrors.ovh.net/ftp.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz | tar zx -C ~/maven
+  fi
+  popd > /dev/null
+  export M2_HOME=~/maven/apache-maven-3.3.9
+  export PATH=$M2_HOME/bin:$PATH
+}
+
+#
+# Replaces the version defined in sources, usually x.y-SNAPSHOT,
+# by a version identifying the build.
+# The build version is composed of 4 fields, including the semantic version and
+# the build number provided by Travis.
+#
+# Exported variables:
+# - INITIAL_VERSION: version as defined in pom.xml
+# - BUILD_VERSION: version including the build number
+# - PROJECT_VERSION: target Maven version. The name of this variable is important because
+#   it's used by QA when extracting version from Artifactory build info.
+#
+# Example of SNAPSHOT
+# INITIAL_VERSION=6.3-SNAPSHOT
+# BUILD_VERSION=6.3.0.12345
+# PROJECT_VERSION=6.3.0.12345
+#
+# Example of RC
+# INITIAL_VERSION=6.3-RC1
+# BUILD_VERSION=6.3.0.12345
+# PROJECT_VERSION=6.3-RC1
+#
+# Example of GA
+# INITIAL_VERSION=6.3
+# BUILD_VERSION=6.3.0.12345
+# PROJECT_VERSION=6.3
+#
+function fixBuildVersion {
+  export INITIAL_VERSION=$(maven_expression "project.version")
+
+  # remove suffix -SNAPSHOT or -RC
+  without_suffix=$(echo $INITIAL_VERSION | sed "s/-.*//g")
+
+  IFS=$'.'
+  fields_count=$(echo $without_suffix | wc -w)
+  unset IFS
+  if [ $fields_count -lt 3 ]; then
+    export BUILD_VERSION="$without_suffix.0.$TRAVIS_BUILD_NUMBER"
+  else
+    export BUILD_VERSION="$without_suffix.$TRAVIS_BUILD_NUMBER"
+  fi
+
+  if [[ "${INITIAL_VERSION}" == *"-SNAPSHOT" ]]; then
+    # SNAPSHOT
+    export PROJECT_VERSION=$BUILD_VERSION
+    grep --include={*.properties,pom.xml} -rnl './' -e "$INITIAL_VERSION" | xargs -i@ sed -i "s/$INITIAL_VERSION/$PROJECT_VERSION/g" @
+  else
+    # not a SNAPSHOT: milestone, RC or GA
+    export PROJECT_VERSION=$INITIAL_VERSION
+  fi
+
+  echo "Build Version  : $BUILD_VERSION"
+  echo "Project Version: $PROJECT_VERSION"
+}
+
+#
+# Configure Maven settings and install some script utilities
+#
+function configureTravis {
+  mkdir ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v33 | tar zx --strip-components 1 -C ~/.local
+  source ~/.local/bin/install
+}
+configureTravis
+
+case "$TARGET" in
+
+BUILD)
+
+  installJdk8
+  installMaven
+  fixBuildVersion
+
+  # Minimal Maven settings
+  export MAVEN_OPTS="-Xmx1G -Xms128m"
+  MAVEN_ARGS="-Dmaven.test.redirectTestOutputToFile=false -Djava.net.preferIPv4Stack=true -Dsurefire.useFile=false -B -e -V -DbuildVersion=$BUILD_VERSION"
+
+  if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    echo 'Build and analyze master'
+
+    # Fetch all commit history so that SonarQube has exact blame information
+    # for issue auto-assignment
+    # This command can fail with "fatal: --unshallow on a complete repository does not make sense"
+    # if there are not enough commits in the Git repository (even if Travis executed git clone --depth 50).
+    # For this reason errors are ignored with "|| true"
+    git fetch --unshallow || true
+
+    mvn clean package jacoco:report sonar:sonar \
+          $MAVEN_ARGS \
+          -Pjacoco -Djacoco.includes=org.ligoj.app.* \
+          -Dsonar.host.url=$SONAR_HOST_URL \
+          -Dsonar.organization=ligoj-github \
+          -Dsonar.login=$SONAR_TOKEN \
+          -Dsonar.projectVersion=$PROJECT_VERSION \
+          -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+          -Dsonar.github.oauth=$GITHUB_TOKEN \
+          -Dmaven.javadoc.skip=true \
+          -Dmaven.ut.reuseForks=true -Dmaven.it.reuseForks=false
+
+    mvn coveralls:report \
+          $MAVEN_ARGS
+
+  elif [[ "$TRAVIS_BRANCH" == "branch-"* ]] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    echo 'Build release branch'
+
+    mvn install $MAVEN_ARGS
+
+  elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN:-}" ]; then
+    echo 'Build and analyze internal pull request'
+
+    mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar \
+        $MAVEN_ARGS \
+        -Dsource.skip=true \
+        -Pdeploy-sonarsource \
+        -Dsonar.analysis.mode=preview \
+        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+        -Dsonar.github.oauth=$GITHUB_TOKEN \
+        -Dsonar.host.url=$SONAR_HOST_URL \
+        -Dsonar.login=$SONAR_TOKEN
+
+  else
+    echo 'Build feature branch or external pull request'
+
+    mvn install $MAVEN_ARGS
+  fi
+
+  ;;
+
+*)
+  echo "Unexpected TARGET value: $TARGET"
+  exit 1
+  ;;
+
+esac
