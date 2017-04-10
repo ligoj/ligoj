9e098625840947901357f219ecae3920e9c34d27
Fabrice Daugan <fabrice.daugan@gmail.com>
2017-04-10 12:18:52 +0200

    Handle rich confirmation messages

--- app-web/src/main/webapp/lib/bootbox/bootbox.mod.js
+++ app-web/src/main/webapp/lib/bootbox/bootbox.mod.js
@@ -5,8 +5,8 @@ define(['jquery', 'cascade', 'i18n!bootbox/nls/bootbox-messages', 'bootbox/bootb
 	/**
 	 * Display a prompt for a delete confirmation.
 	 * @param {Function} callback confirmed callback.
-	 * @param {String} optional deletion parameter used for template.
-	 * @param {String} optional raw message to add.
+	 * @param {String} param optional deletion parameter used for template.
+	 * @param {String} param2 optional html message to add.
 	 */
 	module.confirmDelete = function (callback, param, param2) {
 		var message = Handlebars.compile($cascade.$messages['deleteConfirm' + (param ? 'Param' : '')])(param) + (param2 || '');
