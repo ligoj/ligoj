b89991596365cc9611fdf2abdc4dbc5a2bcbd96b
Fabrice Daugan <fabrice.daugan@gmail.com>
2017-08-20 22:13:44 +0200

    Switch cron-gen to jqcron implementation

---
+++ app-ui/src/main/webapp/lib/jqcron/nls/de/messages.js
@@ -0,0 +1,21 @@
+define({
+	"empty": '-Alle-',
+	"name_minute": 'Minute',
+	"name_hour": 'Stunde',
+	"name_day": 'Tag',
+	"name_week": 'Woche',
+	"name_month": 'Monat',
+	"name_year": 'Jahr',
+	"text_period": 'Jede <b />',
+	"text_mins": 'um <b /> minuten nach der Stunde',
+	"text_time": 'um <b />:<b />:<b />',
+	"text_dow": 'am <b />',
+	"text_month": 'im <b />',
+	"text_dom": 'am <b />',
+	"error1": 'Der Tag %s wird nicht unterst&uuml;tzt!',
+	"error2": 'Ung&uuml;ltige Anzahl an Werten!',
+	"error3": 'Das jquery_element sollte in den jqCron-Einstellungen gesetzt werden!',
+	"error4": 'Ung&uuml;ltiger Ausdruck',
+	"weekdays": ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag'],
+	"months": ['J&auml;nner', 'Februar', 'M&auml;rz', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']
+});
