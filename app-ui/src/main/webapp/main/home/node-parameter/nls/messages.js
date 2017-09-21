1958ceed3fa629e86e1f40d9f5c4774e88c3c880
Fabrice Daugan <fabrice.daugan@gmail.com>
2017-09-21 16:45:04 +0200

    Prevent empty parameter value pollution on update

--- app-ui/src/main/webapp/main/home/node-parameter/nls/messages.js
+++ app-ui/src/main/webapp/main/home/node-parameter/nls/messages.js
@@ -2,7 +2,10 @@ define({
 	"root": {
 		"secured-untouched": "Value is secured, not empty and cannot be displayed. Click here to enable and override the value in this input.",
 		"secured-touched": "Secured valued will be replaced by this new value.",
-		"secured-empty": "Value will be secured in the database and will never be exposed outisde the server."
+		"secured-empty": "Value will be secured in the database and will never be exposed outisde the server.",
+		"error" : {
+			"used-parameter-value": "This mandatory parameter cannot be deleted, it is used by {{subscriptions}} subscriptions"
+		}
 	},
 	"fr": true
 });
