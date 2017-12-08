1bf42135b0addda834937950cd67e0edc9319893
fabdouglas <fabrice.daugan@gmail.com>
2017-12-08 10:48:27 +0100

    Remove extra space before the unit

--- app-ui/src/main/webapp/lib/format/nls/format-messages.js
+++ app-ui/src/main/webapp/lib/format/nls/format-messages.js
@@ -2,8 +2,8 @@ define({
 	root : {
 		'shortdateMomentJs' : 'YYYY/MM/DD',
 		'unit-size' : 'o',
-		'format-size' : '{{[0]}} {{[1]}}{{[2]}}',
-		'format-cost' : '{{[0]}} {{[1]}}{{[2]}}'
+		'format-size' : '{{[0]}}{{[1]}}{{[2]}}',
+		'format-cost' : '{{[0]}}{{[1]}}{{[2]}}'
 	},
 	fr : true,
 	es : true
