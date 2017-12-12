3b6ea0426cda667b41cf8ca3c7d21a25b5ea9e68
fabdouglas <fabrice.daugan@gmail.com>
2017-12-12 23:17:06 +0100

    Add UI delegated format

--- app-ui/src/main/webapp/lib/format/format.mod.js
+++ app-ui/src/main/webapp/lib/format/format.mod.js
@@ -20,16 +20,17 @@ define(['jquery', 'i18n!format/nls/format-messages', 'moment.mod'], function ($,
 
 		/**
 		 * Bytes to human readable string. Trailing decimal '0' are removed
-		 * @param {number} bytes     The amount of bytes.
-		 * @param {number} digits    Amount of digit. Default is 3 : When value is 3, you can get "6.23MB",  "62.2MB" or  "623MB".
-		 *                           When value is 2 (minimal value), you can get "6.2MB", "62MB" or "0.6MB".
-		 *                           When value is 4, you can get "6.234MB", "62.34MB" or "623.4MB".
-		 * @param {number} unit      The unit name. For sample : "$", "B", "o".
-		 * @param {number} pow       The 1K value. For samples : 1000 or 1024.
-		 * @param {string} format    The format used to build the final value accepting an array having this structure : [ value, power, unit].
-		 *                           For sample : [1.23, 'M','£'].
-		 * @param {string} clazz     Optional class to apply to unit. ay be null.
-		 * @return {string}          Human readable string
+		 * @param {number} bytes           The amount of bytes.
+		 * @param {number} digits          Amount of digit. Default is 3 : When value is 3, you can get "6.23MB",  "62.2MB" or  "623MB".
+		 *                                 When value is 2 (minimal value), you can get "6.2MB", "62MB" or "0.6MB".
+		 *                                 When value is 4, you can get "6.234MB", "62.34MB" or "623.4MB".
+		 * @param {number} unit            The unit name. For sample : "$", "B", "o".
+		 * @param {number} pow             The 1K value. For samples : 1000 or 1024.
+		 * @param {string|function} format Either the format used to build the final value accepting an array having this structure : [ value, power, unit].
+		 *                                 For sample : [1.23, 'M','£']. Either the callback function. 
+		 *                                 When defined, this callback receive "value", "weight", "unit" parameters.
+		 * @param {string} clazz           Optional class to apply to unit. ay be null.
+		 * @return {string}                Human readable string
 		 */
 		formatUnit: function (bytes, digits, sizes, unit, pow, format, clazz) {
 			bytes = (Math.round(bytes * pow) / pow) || 0;
@@ -38,7 +39,7 @@ define(['jquery', 'i18n!format/nls/format-messages', 'moment.mod'], function ($,
 				var s = sizes;
 				var e = Math.max(0, Math.floor(Math.log(bytes) / Math.log(pow)));
 				var decimals;
-				if (e <= 1) { 
+				if (e <= 1 && bytes < 10 * pow) { 
 					// No need to format
 					decimals = 0;
 					e = 0;
@@ -58,28 +59,36 @@ define(['jquery', 'i18n!format/nls/format-messages', 'moment.mod'], function ($,
 						decimals = digits - 1;
 					}
 				}
-				return Handlebars.compile(format)({
-					value : value.toFixed(decimals).replace(decimals == 1 ? /\.0/ : /\.00/, ''),
-					weight: sizes[e],
-					unit: unit,
-					preUnit: clazz ? '<span class="' + clazz +'">' : '',
-					postUnit: clazz ? '</span>' : '',
-				});
+				value = value.toFixed(decimals).replace(decimals == 1 ? /\.0/ : /\.00/, '');
+				if (typeof format === 'function') {
+					format(value, sizes[e], unit);
+				} else {
+					return Handlebars.compile(format)({
+						value : value,
+						weight: sizes[e],
+						unit: unit,
+						preUnit: clazz ? '<span class="' + clazz +'">' : '',
+						postUnit: clazz ? '</span>' : '',
+					});
+				}
 			}
 			return '';
 		},
 
 		/**
 		 * Bytes to human readable string. Trailing decimal '0' are removed
-		 * @param {number} bytes    The amount of bytes.
-		 * @param {number} digits   Amount of digit. Default is 3 : When value is 3, you can get "6.23MB",  "62.2MB" or  "623MB".
-		 *                          When value is 2 (minimal value), you can get "6.2MB", "62MB" or "0.6MB".
-		 *                          When value is 4, you can get "6.234MB", "62.34MB" or "623.4MB".
-		 * @param {string} clazz    Optional class to apply to unit. ay be null.
-		 * @return {string}         Human readable string
+		 * @param {number} bytes           The amount of bytes.
+		 * @param {number} digits          Amount of digit. Default is 3 : When value is 3, you can get "6.23MB",  "62.2MB" or  "623MB".
+		 *                                 When value is 2 (minimal value), you can get "6.2MB", "62MB" or "0.6MB".
+		 *                                 When value is 4, you can get "6.234MB", "62.34MB" or "623.4MB".
+		 * @param {string} clazz           Optional class to apply to unit. ay be null.
+		 * @param {string|function} format Either the format used to build the final value accepting an array having this structure : [ value, power, unit].
+		 *                                 For sample : [1.23, 'M','£']. Either the callback function. 
+		 *                                 When defined, this callback receive "value", "weight", "unit" parameters.
+		 * @return {string}                Human readable string.
 		 */
-		formatCost: function (bytes, digits, currency, clazz) {
-			return formatManager.formatUnit(bytes, digits, ['', 'K', 'M', 'B', 'T'], currency, 1000, messages['format-cost'], clazz);
+		formatCost: function (bytes, digits, currency, clazz, format) {
+			return formatManager.formatUnit(bytes, digits, ['', 'K', 'M', 'B', 'T'], currency, 1000, format || messages['format-cost'], clazz);
 		},
 
 		/**
@@ -89,7 +98,7 @@ define(['jquery', 'i18n!format/nls/format-messages', 'moment.mod'], function ($,
 		 *                          When value is 2 (minimal value), you can get "6.2MB", "62MB" or "0.6MB".
 		 *                          When value is 4, you can get "6.234MB", "62.34MB" or "623.4MB".
 		 * @param {string} clazz    Optional class to apply to unit. ay be null.
-		 * @return {string}         Human readable string
+		 * @return {string}         Human readable string.
 		 */
 		formatSize: function (bytes, digits, clazz) {
 			var unit = messages['unit-size'];
