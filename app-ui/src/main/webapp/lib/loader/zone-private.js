9e151c5fa23f0d98506358f098035c34aa2b03a8
Fabrice Daugan <fabrice.daugan@gfi.fr>
2018-02-03 11:45:10 +0100

    Drop sidebartree component

--- app-ui/src/main/webapp/lib/loader/zone-private.js
+++ app-ui/src/main/webapp/lib/loader/zone-private.js
@@ -1,5 +1,5 @@
 define(['moment.mod', 'zone-protected', 'datatables.mod', 'form',
-	'bootstrap.mod','bootstrap-datepicker.mod', 'bootbox.mod', 'sidebartree.mod', 'select2',
+	'bootstrap.mod','bootstrap-datepicker.mod', 'bootbox.mod', 'select2',
 	'bootstrap-daterangepicker.mod', 'bootstrap-spinner', 'scroll.mod', 'bootstrap-jasny',
 	'bootstrap-timepicker.mod', 'validation.mod', 'l10n.mod', 'material.mod', 'ripples',
 	'formatter', 'text!main/main.html', 'i18n!main/nls/messages'
