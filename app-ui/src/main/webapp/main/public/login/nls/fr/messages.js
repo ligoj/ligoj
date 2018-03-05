7111becef5137e01f5eca8afa4199f20cfba6aeb
Haithem SOUALA <soualahaitem@gmail.com>
2018-03-05 22:52:47 +0100

    UI: Displaying ppolicy feedback

--- app-ui/src/main/webapp/main/public/login/nls/fr/messages.js
+++ app-ui/src/main/webapp/main/public/login/nls/fr/messages.js
@@ -35,7 +35,7 @@ define({
 		"password" : "Mots de passe différents",
 		"password-complexity" : "Mot de passe trop faible<br/>8 ou plus majuscules, minuscules et chiffres",
 		"password-complexity-validation" : "Trop faible, 8 ou plus majuscules, minuscules et chiffres",
-		"password-policy" : "Le nouveau mot de passe est dans l'historique des anciens mots de passe ou le mot de passe est trop jeune pour changer",
+		"password-policy" : "Le nouveau mot de passe est dans l'historique des anciens mots de passe",
 		"login" : "Authentification échouée",
 		"mail" : "Mail non envoyé",
 		"concurrency" : "Compte utilisé ailleurs en ce moment",
