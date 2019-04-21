db2dbb01372dd7e0494a0a7b93e24196a95d9792
Fabrice Daugan <fabrice.daugan@gmail.com>
2019-04-21 13:24:39 +0200

    Remove JUnit 4 dependency

--- app-ui/src/test/java/org/ligoj/boot/web/ApplicationLightTest.java
+++ app-ui/src/test/java/org/ligoj/boot/web/ApplicationLightTest.java
@@ -8,14 +8,14 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * Test class of {@link Application}
  */
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class ApplicationLightTest {
 
