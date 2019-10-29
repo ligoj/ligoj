acc7575d83f44d28ef188e296e31ea18ffc2b91d
Fabrice Daugan <fabrice.daugan@gmail.com>
2019-10-29 13:39:43 +0100

    Update Maven to 3.6.2

--- travis.sh
+++ travis.sh
@@ -10,12 +10,12 @@ function installMaven {
   echo "Setup Maven"
   mkdir -p ~/maven
   pushd ~/maven > /dev/null
-  if [ ! -d "apache-maven-3.6.0" ]; then
-    echo "Download Maven 3.6.0"
-    curl -sSL http://apache.mirrors.ovh.net/ftp.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz | tar zx -C ~/maven
+  if [ ! -d "apache-maven-3.6.2" ]; then
+    echo "Download Maven 3.6.2"
+    curl -sSL http://apache.mirrors.ovh.net/ftp.apache.org/dist/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.tar.gz | tar zx -C ~/maven
   fi
   popd > /dev/null
-  export M2_HOME=~/maven/apache-maven-3.6.0
+  export M2_HOME=~/maven/apache-maven-3.6.2
   export PATH=$M2_HOME/bin:$PATH
   echo '<settings><profiles><profile><id>spring-milestone</id><repositories>' > $M2_HOME/conf/settings.xml
   echo '<repository><id>spring-milestone</id><url>http://repo.spring.io/milestone/</url></repository>' >> $M2_HOME/conf/settings.xml
