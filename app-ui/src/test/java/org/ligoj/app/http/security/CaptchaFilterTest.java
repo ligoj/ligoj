d1646c0ac6cf4d647a63f6cbb75f73eaddc7648a
Fabrice Daugan <fabrice.daugan@gmail.com>
2020-04-04 18:18:03 +0200

    Lint JSE14

--- app-ui/src/test/java/org/ligoj/app/http/security/CaptchaFilterTest.java
+++ app-ui/src/test/java/org/ligoj/app/http/security/CaptchaFilterTest.java
@@ -9,7 +9,6 @@ import java.nio.charset.StandardCharsets;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -27,17 +26,17 @@ import cn.apiclub.captcha.Captcha;
 public class CaptchaFilterTest {
 
 	@Test
-	public void testJavax() throws ServletException {
-		new CaptchaFilter().init(null);
-		new CaptchaFilter().destroy();
+	void testJavax() throws ServletException {
+		new CacheBustingFilter().init(null);
+		new CacheBustingFilter().destroy();
 	}
 
 	@Test
-	public void testDoFilterNoSession() throws ServletException, IOException {
-		final HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
-		final HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
-		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		final ServletOutputStream out = new DelegatingServletOutputStream(baos);
+	void testDoFilterNoSession() throws ServletException, IOException {
+		final var request = Mockito.mock(HttpServletRequest.class);
+		final var response = Mockito.mock(HttpServletResponse.class);
+		final var baos = new ByteArrayOutputStream();
+		final var out = new DelegatingServletOutputStream(baos);
 		Mockito.when(response.getOutputStream()).thenReturn(out);
 		new CaptchaFilter().doFilter(request, response, null);
 		Mockito.verify(response).setStatus(HttpServletResponse.SC_BAD_REQUEST);
@@ -45,12 +44,12 @@ public class CaptchaFilterTest {
 	}
 
 	@Test
-	public void testDoFilterNoCaptcha() throws ServletException, IOException {
-		final HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
-		final HttpSession session = Mockito.mock(HttpSession.class);
-		final HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
-		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		final ServletOutputStream out = new DelegatingServletOutputStream(baos);
+	void testDoFilterNoCaptcha() throws ServletException, IOException {
+		final var request = Mockito.mock(HttpServletRequest.class);
+		final var session = Mockito.mock(HttpSession.class);
+		final var response = Mockito.mock(HttpServletResponse.class);
+		final var baos = new ByteArrayOutputStream();
+		final var out = new DelegatingServletOutputStream(baos);
 		Mockito.when(response.getOutputStream()).thenReturn(out);
 		Mockito.when(request.getSession(false)).thenReturn(session);
 		new CaptchaFilter().doFilter(request, response, null);
@@ -59,16 +58,16 @@ public class CaptchaFilterTest {
 	}
 
 	@Test
-	public void testDoFilterInvalidCaptcha() throws ServletException, IOException {
-		final HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
-		final HttpSession session = Mockito.mock(HttpSession.class);
-		final HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
-		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		final ServletOutputStream out = new DelegatingServletOutputStream(baos);
+	void testDoFilterInvalidCaptcha() throws ServletException, IOException {
+		final var request = Mockito.mock(HttpServletRequest.class);
+		final var session = Mockito.mock(HttpSession.class);
+		final var response = Mockito.mock(HttpServletResponse.class);
+		final var baos = new ByteArrayOutputStream();
+		final var out = new DelegatingServletOutputStream(baos);
 		Mockito.when(response.getOutputStream()).thenReturn(out);
 		Mockito.when(request.getSession(false)).thenReturn(session);
 		Mockito.when(request.getHeader(CaptchaFilter.CAPTCHA_HEADER)).thenReturn("some");
-		final Captcha captcha = new Captcha.Builder(200, 50).addText(() -> "check").build();
+		final var captcha = new Captcha.Builder(200, 50).addText(() -> "check").build();
 		Mockito.when(session.getAttribute(Captcha.NAME)).thenReturn(captcha);
 		new CaptchaFilter().doFilter(request, response, null);
 		Mockito.verify(response).setStatus(HttpServletResponse.SC_BAD_REQUEST);
@@ -76,14 +75,14 @@ public class CaptchaFilterTest {
 	}
 
 	@Test
-	public void testDoFilter() throws ServletException, IOException {
-		final HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
-		final HttpSession session = Mockito.mock(HttpSession.class);
-		final HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
-		final FilterChain chain = Mockito.mock(FilterChain.class);
+	void testDoFilter() throws ServletException, IOException {
+		final var request = Mockito.mock(HttpServletRequest.class);
+		final var session = Mockito.mock(HttpSession.class);
+		final var response = Mockito.mock(HttpServletResponse.class);
+		final var chain = Mockito.mock(FilterChain.class);
 		Mockito.when(request.getSession(false)).thenReturn(session);
 		Mockito.when(request.getHeader(CaptchaFilter.CAPTCHA_HEADER)).thenReturn("check");
-		final Captcha captcha = new Captcha.Builder(200, 50).addText(() -> "check").build();
+		final var captcha = new Captcha.Builder(200, 50).addText(() -> "check").build();
 
 		Mockito.when(session.getAttribute(Captcha.NAME)).thenReturn(captcha);
 		new CaptchaFilter().doFilter(request, response, chain);
