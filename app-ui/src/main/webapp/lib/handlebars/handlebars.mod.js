6f7d840f97bcc6145a96943051618cf80187829b
Fabrice Daugan <fabrice.daugan@gmail.com>
2023-07-12 16:22:28 +0200

    Add "date" helper to handlebars

--- app-ui/src/main/webapp/lib/handlebars/handlebars.mod.js
+++ app-ui/src/main/webapp/lib/handlebars/handlebars.mod.js
@@ -8,4 +8,16 @@ define(["handlebars/handlebars"], function(handlebars) {
 	Handlebars.registerHelper('length', function(assignments) {
 		return assignments.length;
 	});
+	var SIMPLE_FORMATS  = {
+        YYYY : d => new Date().getFullYear(),
+        MM : d => new Date().getMonths()+1,
+        dd : d => new Date().getDate(),
+        HH : d => new Date().getHours(),
+        mm : d => new Date().getMinutes()
+	};
+	Handlebars.registerHelper('date', function(format) {
+        return (format && moment)
+        ? moment(new Date()).format(format)
+        : (format && SIMPLE_FORMATS[format](new Date()) || new Date().toISOString());
+	});
 });
