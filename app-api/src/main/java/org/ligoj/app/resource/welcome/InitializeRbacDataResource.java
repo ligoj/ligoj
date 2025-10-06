e32eb5b942043088826185cd21c761cb9411f4c6
Fabrice Daugan <fabrice.daugan@gmail.com>
2025-10-06 16:36:42 +0200

    feat: support of initial user/role/token

--- app-api/src/main/java/org/ligoj/app/resource/welcome/InitializeRbacDataResource.java
+++ app-api/src/main/java/org/ligoj/app/resource/welcome/InitializeRbacDataResource.java
@@ -3,23 +3,76 @@
  */
 package org.ligoj.app.resource.welcome;
 
-import java.util.Arrays;
-import java.util.List;
-
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
 import org.ligoj.app.iam.model.DelegateOrg;
 import org.ligoj.app.model.DelegateNode;
 import org.ligoj.bootstrap.core.plugin.FeaturePlugin;
+import org.ligoj.bootstrap.dao.system.SystemApiTokenRepository;
+import org.ligoj.bootstrap.dao.system.SystemRoleRepository;
+import org.ligoj.bootstrap.dao.system.SystemUserRepository;
 import org.ligoj.bootstrap.model.system.SystemAuthorization;
 import org.ligoj.bootstrap.model.system.SystemRole;
 import org.ligoj.bootstrap.model.system.SystemRoleAssignment;
 import org.ligoj.bootstrap.model.system.SystemUser;
+import org.ligoj.bootstrap.resource.system.api.ApiTokenResource;
+import org.ligoj.bootstrap.resource.system.configuration.ConfigurationResource;
+import org.ligoj.bootstrap.resource.system.security.AuthorizationEditionVo;
+import org.ligoj.bootstrap.resource.system.security.RoleResource;
+import org.ligoj.bootstrap.resource.system.security.SystemRoleVo;
+import org.ligoj.bootstrap.resource.system.user.SystemUserEditionVo;
+import org.ligoj.bootstrap.resource.system.user.UserResource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Initialize during the RBAC data on installation.
  */
+@Log4j2
 @Component
 public class InitializeRbacDataResource implements FeaturePlugin {
+
+	/**
+	 * Initial Ligoj user and token created during the first installation.
+	 */
+	private static final String INITIAL_USER_NAME_PROPERTY = "ligoj.initial.user.name";
+	private static final String INITIAL_ROLE_NAME_PROPERTY = "ligoj.initial.user.role";
+	private static final String INITIAL_TOKEN_NAME_PROPERTY = "ligoj.initial.user.token.name";
+	private static final String INITIAL_TOKEN_VALUE_PROPERTY = "ligoj.initial.user.token.value";
+	private static final String INITIAL_USER_ACTION_PROPERTY = "ligoj.initial.user.action";
+
+	private static final String INITIAL_USER_ACTION_RESET = "reset";
+	private static final String INITIAL_USER_ACTION_INIT = "init";
+
+	private static final String DEFAULT_USER_NAME = "ligoj-admin";
+	private static final String DEFAULT_ROLE_NAME = "ADMIN";
+	private static final String DEFAULT_TOKEN_NAME = "auto-install";
+
+	@Autowired
+	private ConfigurationResource configuration;
+
+	@Autowired
+	private SystemRoleRepository roleRepository;
+
+	@Autowired
+	private UserResource userResource;
+
+	@Autowired
+	private RoleResource roleResource;
+
+	@Autowired
+	private ApiTokenResource apiTokenResource;
+
+	@Autowired
+	protected SystemApiTokenRepository apiTokenRepository;
+
+	@Autowired
+	private SystemUserRepository userRepository;
 
 	@Override
 	public String getKey() {
@@ -35,6 +88,83 @@ public class InitializeRbacDataResource implements FeaturePlugin {
 	@Override
 	public String getName() {
 		return "Welcome Data RBAC";
+	}
+
+	@Override
+	public void install() throws GeneralSecurityException {
+		var customUser = configuration.get(INITIAL_USER_NAME_PROPERTY, DEFAULT_USER_NAME);
+		var customRole = configuration.get(INITIAL_ROLE_NAME_PROPERTY, DEFAULT_ROLE_NAME);
+		var tokenName = configuration.get(INITIAL_TOKEN_NAME_PROPERTY, DEFAULT_TOKEN_NAME);
+		var tokenValue = configuration.get(INITIAL_TOKEN_VALUE_PROPERTY);
+		var action = configuration.get(INITIAL_USER_ACTION_PROPERTY);
+		if (!INITIAL_USER_ACTION_INIT.equals(action) && !INITIAL_USER_ACTION_RESET.equals(action)) {
+			return;
+		}
+
+		// Create the role as needed
+		var role = roleRepository.findByName(customRole);
+		final int roleId;
+		if (role == null) {
+			var roleVo = new SystemRoleVo();
+			roleVo.setName(customRole);
+			var uiAuthorization = new AuthorizationEditionVo();
+			uiAuthorization.setPattern(".*");
+			uiAuthorization.setType(SystemAuthorization.AuthorizationType.UI);
+
+			var apiAuthorization = new AuthorizationEditionVo();
+			apiAuthorization.setPattern(".*");
+			apiAuthorization.setType(SystemAuthorization.AuthorizationType.API);
+
+			var authorizations = List.of(uiAuthorization, apiAuthorization);
+			roleVo.setAuthorizations(authorizations);
+			roleId = roleResource.create(roleVo);
+			log.info("[INIT] Role '{}' has been created", customRole);
+		} else {
+			roleId = role.getId();
+		}
+
+		// Create the user as needed
+		var user = userRepository.findOne(customUser);
+		final var userVo = new SystemUserEditionVo();
+		userVo.setLogin(customUser);
+		userVo.setRoles(Set.of(roleId));
+		if (user == null) {
+			userResource.create(userVo);
+			log.info("[INIT] User '{}' has been created with role '{}'", customUser, customRole);
+		} else if (user.getRoles().stream().noneMatch(a -> a.getRole().getId() == roleId)) {
+			userResource.create(userVo);
+			log.info("[INIT] User '{}' has received role '{}'", customUser, customRole);
+		}
+
+		// Create the user token as needed
+		var token = apiTokenRepository.findByUserAndName(customUser, tokenName);
+		String newTokenValue = null;
+		if (token == null) {
+			if (StringUtils.isBlank(tokenValue)) {
+				// Generate a new token if it does not exist for this user
+				newTokenValue = apiTokenResource.create(customUser, tokenName).getId();
+			} else {
+				// Create a token from a provided token value
+				apiTokenResource.create(customUser, tokenName, tokenValue).getId();
+			}
+		} else if (INITIAL_USER_ACTION_RESET.equals(action)) {
+			if (StringUtils.isBlank(tokenValue)) {
+				// Generate a new token if it does not exist for this user
+				newTokenValue = apiTokenResource.update(customUser, tokenName);
+			} else {
+				// Create a token from a provided token value
+				apiTokenResource.update(customUser, tokenName, tokenValue);
+			}
+		} else {
+			// In non-"reset" mode, no logs are produced
+			return;
+		}
+		if (newTokenValue == null) {
+			log.info("[INIT] Token '{}' has been set for initial user '{}' to desired value", tokenName, customUser);
+		} else {
+			log.info("[INIT] Token '{}' has been set for initial user '{}' to value: {}", tokenName, customUser, newTokenValue);
+		}
+
 	}
 
 }
--- app-api/src/test/java/org/ligoj/app/resource/welcome/InitializeRbacDataResourceTest.java
+++ app-api/src/test/java/org/ligoj/app/resource/welcome/InitializeRbacDataResourceTest.java
@@ -7,12 +7,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.ligoj.app.AbstractServerTest;
+import org.ligoj.bootstrap.dao.system.AuthorizationRepository;
+import org.ligoj.bootstrap.dao.system.SystemRoleRepository;
+import org.ligoj.bootstrap.dao.system.SystemUserRepository;
+import org.ligoj.bootstrap.model.system.SystemAuthorization;
 import org.ligoj.bootstrap.model.system.SystemUser;
+import org.ligoj.bootstrap.resource.system.api.ApiTokenResource;
+import org.ligoj.bootstrap.resource.system.configuration.ConfigurationResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.security.GeneralSecurityException;
 
 /**
  * Test class of {@link InitializeRbacDataResource}
@@ -25,6 +33,22 @@ class InitializeRbacDataResourceTest extends AbstractServerTest {
 
 	@Autowired
 	private InitializeRbacDataResource resource;
+
+	@Autowired
+	private ConfigurationResource configuration;
+
+
+	@Autowired
+	private SystemRoleRepository roleRepository;
+
+	@Autowired
+	private AuthorizationRepository authorizationRepository;
+
+	@Autowired
+	private SystemUserRepository userRepository;
+
+	@Autowired
+	private ApiTokenResource tokenResource;
 
 	@Test
 	void getInstalledEntities() {
@@ -40,4 +64,60 @@ class InitializeRbacDataResourceTest extends AbstractServerTest {
 	void getName() {
 		Assertions.assertEquals("Welcome Data RBAC", resource.getName());
 	}
+
+	@Test
+	void installWithoutInitialization() throws GeneralSecurityException {
+		resource.install();
+	}
+
+	@Test
+	void installInit() throws GeneralSecurityException {
+		configuration.put("ligoj.initial.user.action", "init");
+		resource.install();
+	}
+
+	@Test
+	void installReset() throws GeneralSecurityException {
+		configuration.put("ligoj.initial.user.action", "reset");
+		configuration.put("ligoj.initial.user.role", "NEW_ROLE");
+		configuration.put("ligoj.initial.user.name", "NEW_USER");
+		configuration.put("ligoj.initial.user.token.name", "TOKEN_NAME");
+
+		resource.install();
+
+		var newRole = roleRepository.findByName("NEW_ROLE");
+		Assertions.assertNotNull(newRole);
+		var uiAuth = authorizationRepository.findAllByLogin("NEW_USER", SystemAuthorization.AuthorizationType.UI);
+		var apiAuth = authorizationRepository.findAllByLogin("NEW_USER", SystemAuthorization.AuthorizationType.UI);
+		Assertions.assertEquals(".*", uiAuth.getFirst().getPattern());
+		Assertions.assertEquals(".*", apiAuth.getFirst().getPattern());
+
+		// Init again, with forced token value
+		configuration.put("ligoj.initial.user.token.value", "RANDOM_TEST");
+		resource.install();
+		Assertions.assertEquals("RANDOM_TEST", tokenResource.getToken("TOKEN_NAME", "NEW_USER"));
+
+		// Init again, with another token mode
+		configuration.put("ligoj.initial.user.token.name", "TOKEN_NAME2");
+		configuration.put("ligoj.initial.user.token.value", "RANDOM_TEST2");
+		resource.install();
+		Assertions.assertEquals("RANDOM_TEST2", tokenResource.getToken("TOKEN_NAME2", "NEW_USER"));
+
+		// Init again, with created role and updated token value to be generated
+		configuration.put("ligoj.initial.user.token.name", "TOKEN_NAME");
+		configuration.put("ligoj.initial.user.token.value", null);
+		resource.install();
+		var newTokenValue = tokenResource.getToken("TOKEN_NAME", "NEW_USER");
+		Assertions.assertNotEquals("RANDOM_TEST", newTokenValue);
+
+		// Init again, with init mode
+		configuration.put("ligoj.initial.user.action", "init");
+		configuration.put("ligoj.initial.user.role", "NEW_ROLE2");
+		em.flush();
+		em.clear();
+		userRepository.findOne("NEW_USER");
+		resource.install();
+		Assertions.assertEquals(newTokenValue, tokenResource.getToken("TOKEN_NAME", "NEW_USER"));
+	}
+
 }
