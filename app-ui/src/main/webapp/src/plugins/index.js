010d5fe7d2d49400adbd54c5ea92788a6ea948cd
Fabrice Daugan <fabrice.daugan@gmail.com>
2026-05-29 08:34:37 +0200

    lint: remove converted plugins from built-in registry

--- app-ui/src/main/webapp/src/plugins/index.js
+++ app-ui/src/main/webapp/src/plugins/index.js
@@ -1,9 +1,8 @@
 import registry from './registry.js'
 import inboxSqlDef from './inbox-sql/index.js'
-import provDef from './prov/index.js'
 
 export function registerBuiltinPlugins() {
-  const plugins = [inboxSqlDef, provDef]
+  const plugins = [inboxSqlDef]
   for (const def of plugins) {
     registry.register(def.id, def)
   }
--- app-ui/src/main/webapp/src/plugins/prov/index.js
+++
@@ -1,9 +0,0 @@
-import ProvPlugin from './ProvPlugin.vue'
-
-export default {
-  id: 'service:prov',
-  label: 'Provisioning',
-  component: ProvPlugin,
-  install() {},
-  meta: { icon: 'mdi-cloud-outline', color: 'teal' },
-}
